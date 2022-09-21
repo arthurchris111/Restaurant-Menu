bae7035e392e9fb36e2ec65500714cbb45b6e0b7
christian <arthurchristian111@gmail.com>
2022-09-21 13:35:23 +0000

    added an array of object containing food items

---
+++ assets/app.js
@@ -0,0 +1,100 @@
+const menu = [{
+        id: 1,
+        title: ' ButterMilk Pancakes',
+        category: 'breakfast',
+        price: 6.99,
+        img: "./assets/images/pancake.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 2,
+        title: ' Dream Oreo',
+        category: 'shake',
+        price: 15.99,
+        img: "./assets/images/dream.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 3,
+        title: 'Country Delight',
+        category: 'lunch',
+        price: 10.99,
+        img: "./assets/images/country.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 4,
+        title: 'American Special',
+        category: 'lunch',
+        price: 15.99,
+        img: "./assets/images/america.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 5,
+        title: 'Dinner Double',
+        category: 'lunch',
+        price: 10.99,
+        img: "./assets/images/dinner double.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 6,
+        title: 'Godzilla Milkshake',
+        category: 'shake',
+        price: 12.99,
+        img: "./assets/images/godzilla milkshake.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 7,
+        title: 'Special Pizza',
+        category: 'lunch',
+        price: 15.99,
+        img: "./assets/images/pizza.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 8,
+        title: 'Egg Attack',
+        category: 'lunch',
+        price: 11.99,
+        img: "./assets/images/egg attack.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 9,
+        title: 'Burger',
+        category: 'lunch',
+        price: 13.99,
+        img: "./assets/images/burger.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 10,
+        title: 'Samosa',
+        category: 'lunch',
+        price: 6.99,
+        img: "./assets/images/samosa.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+]
