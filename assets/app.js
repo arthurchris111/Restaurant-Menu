1f573f0fd5652f75aff6c11a00167fdbd082078c
christian <arthurchristian111@gmail.com>
2022-09-23 00:31:54 +0000

    added new images

--- assets/app.js
+++ assets/app.js
@@ -40,46 +40,17 @@ const menu = [{
 
     {
         id: 5,
-        title: 'Dinner Double',
-        category: 'lunch',
-        price: 10.99,
-        img: "./assets/images/dinner double.jpg",
-        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
-    quis nostrud exercitation ullamco`
-    },
-
-    {
-        id: 6,
-        title: 'Godzilla Milkshake',
-        category: 'shake',
-        price: 12.99,
-        img: "./assets/images/godzilla milkshake.jpg",
-        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
-    quis nostrud exercitation ullamco`
-    },
-
-    {
-        id: 7,
         title: 'Special Pizza',
         category: 'lunch',
         price: 15.99,
         img: "./assets/images/pizza.jpg",
         description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
     quis nostrud exercitation ullamco`
+
     },
 
     {
-        id: 8,
-        title: 'Egg Attack',
-        category: 'lunch',
-        price: 11.99,
-        img: "./assets/images/egg attack.jpg",
-        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
-    quis nostrud exercitation ullamco`
-    },
-
-    {
-        id: 9,
+        id: 6,
         title: 'Burger',
         category: 'lunch',
         price: 13.99,
@@ -89,7 +60,7 @@ const menu = [{
     },
 
     {
-        id: 10,
+        id: 8,
         title: 'Samosa',
         category: 'lunch',
         price: 6.99,
@@ -97,6 +68,66 @@ const menu = [{
         description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
     quis nostrud exercitation ullamco`
     },
+
+    {
+        id: 8,
+        title: 'French Rice',
+        category: 'lunch',
+        price: 7.99,
+        img: "./assets/images/america.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 9,
+        title: 'German Chicken',
+        category: 'lunch',
+        price: 6.99,
+        img: "./assets/images/country.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
+
+    {
+        id: 10,
+        title: 'Godzilla Milks',
+        category: 'lunch',
+        price: 6.29,
+        img: "./assets/images/dream.jpg",
+        description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, 
+    quis nostrud exercitation ullamco`
+    },
 ]
 
 const sectionCenter = document.getElementById("sectionCenter")
+
+window.addEventListener("DOMContentLoaded", () => {
+    let displayMenu = menu.map((item) => {
+        // console.log(item)
+        return `<div id="sectionCenter"
+                    class = "col col-lg-6 col-md-12 col-sm-12 d-flex justify-content-md-center justify-content-sm-center" >
+                    <div class="card mb-3" style="max-width: 540px;">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <a href="#"><img src=${item.img} class="img-fluid h-100 rounded-start"
+                                        alt="${item.title}"></a>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between">
+                                        <h6>${item.title}</h6>
+                                        <h6 class="amount">${item.price}</h6> 
+                                    </div>
+                                    <hr class="mt-0 ">
+                                    <p class="text-start">${item.description}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+    })
+    displayMenu = displayMenu.join('')
+    sectionCenter.innerHTML = displayMenu
+    console.log(displayMenu)
+})
